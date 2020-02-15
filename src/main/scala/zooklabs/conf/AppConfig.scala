b03ade0e3770fb435e14abfb451a4c02fd0335f4
BearRebel <bearrebel@zooklabs.com>
2020-02-15 01:02:21 +0000

    format project

--- src/main/scala/zooklabs/conf/AppConfig.scala
+++ src/main/scala/zooklabs/conf/AppConfig.scala
@@ -2,7 +2,7 @@ package zooklabs.conf
 
 import ciris.refined._
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.string.{Uri, Url}
+import eu.timepit.refined.string.Url
 import eu.timepit.refined.types.net.UserPortNumber
 
 case class AppConfig(post: UserPortNumber,
