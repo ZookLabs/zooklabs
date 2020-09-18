529bc1acbd96eb1a2f4614c5e0bddc43f9379e1c
BearRebel <bearrebel@zooklabs.com>
2020-09-18 22:37:18 +0100

    cleanup KeepAliveProgram

--- src/main/scala/zooklabs/program/KeepAliveProgram.scala
+++ src/main/scala/zooklabs/program/KeepAliveProgram.scala
@@ -2,8 +2,8 @@ package zooklabs.program
 
 import cats.effect.{IO, Timer}
 import fs2.Stream
-import org.http4s.Uri
 import org.http4s.client.Client
+import org.http4s.implicits._
 
 import scala.concurrent.duration._
 
@@ -12,7 +12,7 @@ final class KeepAliveProgram(client: Client[IO])(implicit timer: Timer[IO]) {
   def run(): Stream[IO, Unit] = {
     Stream
       .repeatEval(
-        client.statusFromUri(Uri.unsafeFromString("http://api.zooklabs.com/health")).void
+        client.statusFromUri(uri"http://api.zooklabs.com/health").void
       )
       .metered(10.minutes)
   }
