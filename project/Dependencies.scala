4b7967ad933573d45c59ed5277172a39340aaa25
Scala Steward <me@scala-steward.org>
2020-09-25 06:58:37 +0200

    Update fs2-core, fs2-io to 2.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val refined        = "0.9.15"
     val log4Cats       = "1.1.1"
     val ciris          = "1.1.1"
-    val fs2            = "2.4.2"
+    val fs2            = "2.4.4"
     val scalatest      = "3.2.0"
     val googleCloudNio = "0.121.2"
     val flyway         = "6.5.0"
