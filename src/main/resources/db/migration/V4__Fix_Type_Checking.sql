3827ab17e9bc903ba4b29851380ab025622c6db9
BearRebel <56321631+BearRebel@users.noreply.github.com>
2020-09-28 23:56:08 +0100

    Fix comments

--- src/main/resources/db/migration/V4__Fix_Type_Checking.sql
+++ src/main/resources/db/migration/V4__Fix_Type_Checking.sql
@@ -1,8 +1,8 @@
-# update users id to integer
+-- update users id to integer
 alter table users alter column id type integer using id::integer;
 alter table zook alter column owner type integer using owner::integer;
 
-# update zook id to integer
+-- update zook id to integer
 alter table zook alter column id type integer using id::integer;
 alter table block_push alter column zookid type integer using zookid::integer;
 alter table high_jump alter column zookid type integer using zookid::integer;
@@ -10,7 +10,7 @@ alter table hurdles alter column zookid type integer using zookid::integer;
 alter table lap alter column zookid type integer using zookid::integer;
 alter table sprint alter column zookid type integer using zookid::integer;
 
-# update trial name to not null
+-- update trial name to not null
 alter table block_push alter column name set not null;
 alter table high_jump alter column name set not null;
 alter table hurdles alter column name set not null;
