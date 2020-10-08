bdb64b5f2ba95c5c7125d1db986be7e81a3c4def
Shoeman <andrew@rush-radio.co.uk>
2020-10-08 21:34:51 +0100

    Update src/main/resources/db/migration/V6__Add_overall_league_table.sql
    Make zook name not null
    
    Co-authored-by: BearRebel <56321631+BearRebel@users.noreply.github.com>

--- src/main/resources/db/migration/V6__Add_overall_league_table.sql
+++ src/main/resources/db/migration/V6__Add_overall_league_table.sql
@@ -1,7 +1,7 @@
 create table overall_league
 (
     zookid   SMALLSERIAL      not null,
-    name     VARCHAR(25)      null,
+    name     VARCHAR(25)      not null,
     position INT              not null,
     score    DOUBLE PRECISION not null,
     constraint overall_league_pk
