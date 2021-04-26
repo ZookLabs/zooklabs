5651d11cd73147f8a43a2ff2d9462939548971b9
Scala Steward <me@scala-steward.org>
2021-04-26 18:16:02 +0200

    Update flyway-core to 7.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val circe          = "0.13.0"
     val ciris          = "1.2.1"
     val doobie         = "0.10.0"
-    val flyway         = "7.1.1"
+    val flyway         = "7.8.1"
     val fs2            = "2.4.6"
     val googleCloudNio = "0.122.11"
     val http4s         = "0.21.20"
