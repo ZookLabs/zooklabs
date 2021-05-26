2b381cfbabfa9fc8a2efa98db69998d80f42a272
Scala Steward <me@scala-steward.org>
2021-05-26 18:30:49 +0200

    Update scalatest to 3.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val postgres       = "42.2.19"
     val refined        = "0.9.21"
     val scalaJwt       = "5.0.0"
-    val scalatest      = "3.2.6"
+    val scalatest      = "3.2.9"
     val zookcore       = "1.0.2"
   }
 
