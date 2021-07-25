52da89c6d657e47ad9e7367ac87921783075d6ea
Scala Steward <me@scala-steward.org>
2021-07-25 21:11:28 +0200

    Update google-cloud-nio to 0.123.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val doobie         = "1.0.0-M5"
     val flyway         = "7.10.0"
     val fs2            = "3.0.4"
-    val googleCloudNio = "0.123.2"
+    val googleCloudNio = "0.123.3"
     val http4s         = "0.23.0-RC1"
     val log4Cats       = "2.1.1"
     val logback        = "1.2.3"
