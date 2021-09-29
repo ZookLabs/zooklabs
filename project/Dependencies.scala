47acb80e32dcfdbba7247e9e1464c6a6a046687f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-30 00:36:18 +0200

    Update fs2-core, fs2-io to 3.0.6 (#107)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val ciris          = "2.0.1"
     val doobie         = "1.0.0-M5"
     val flyway         = "7.10.0"
-    val fs2            = "3.0.4"
+    val fs2            = "3.0.6"
     val googleCloudNio = "0.123.2"
     val http4s         = "0.23.0-RC1"
     val log4Cats       = "2.1.1"
