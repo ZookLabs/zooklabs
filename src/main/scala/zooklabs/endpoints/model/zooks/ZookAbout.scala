11466be80023cefc517ccbc6efd60f634c9241a0
Shoeman <shoeman@zooklabs.com>
2021-10-03 00:14:57 +0100

    add view counter

--- src/main/scala/zooklabs/endpoints/model/zooks/ZookAbout.scala
+++ src/main/scala/zooklabs/endpoints/model/zooks/ZookAbout.scala
@@ -9,7 +9,9 @@ import zooklabs.endpoints.model.users.UserIdentifier
 final case class ZookAbout(
     owner: Option[UserIdentifier],
     dateCreated: LocalDateTime,
-    dateUploaded: LocalDateTime
+    dateUploaded: LocalDateTime,
+    downloads: Int,
+    views: Int
 )
 
 object ZookAbout {
@@ -17,15 +19,19 @@ object ZookAbout {
   private val dateTimeFormatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy")
 
   implicit val encodeZookAbout: Encoder[ZookAbout] =
-    Encoder.forProduct3(
+    Encoder.forProduct5(
       "owner",
       "dateCreated",
-      "dateUploaded"
+      "dateUploaded",
+      "downloads",
+      "views"
     )(u =>
       (
         u.owner,
         u.dateCreated.format(dateTimeFormatter),
-        u.dateUploaded.format(dateTimeFormatter)
+        u.dateUploaded.format(dateTimeFormatter),
+        u.downloads,
+        u.views
       )
     )
 }
