a4d3c319c935124da2cf823aade33d74bee279b7
Shoeman <shoeman@zooklabs.com>
2021-10-03 00:35:25 +0100

    add download counter

--- src/main/scala/zooklabs/endpoints/StaticEndpoints.scala
+++ src/main/scala/zooklabs/endpoints/StaticEndpoints.scala
@@ -1,14 +1,15 @@
 package zooklabs.endpoints
 
-import java.net.URL
-
+import cats.data.EitherT
 import cats.effect.IO
-import cats.implicits.{toFoldableOps, _}
-import org.typelevel.log4cats.Logger
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
+import org.typelevel.log4cats.Logger
+import zooklabs.repository.ZookRepository
 
-class StaticEndpoints()(implicit logger: Logger[IO]
+import java.net.URL
+
+class StaticEndpoints(zookRepository: ZookRepository)(implicit logger: Logger[IO]
 ) extends Http4sDsl[IO] {
 
   val imageEndpoint: PartialFunction[Request[IO], IO[Response[IO]]] = {
@@ -19,11 +20,18 @@ class StaticEndpoints()(implicit logger: Logger[IO]
   }
 
   val zookEndpoint: PartialFunction[Request[IO], IO[Response[IO]]] = {
-    case GET -> Root / "zooks" / id / name =>
-      StaticFile
-        .fromURL[IO](new URL(s"http://static.zooklabs.com/zooks/$id/$name.zook"))
-        .getOrElseF(NotFound())
+    case GET -> Root / "zooks" / id / name => {
+      (for {
+        id <- EitherT.fromEither[IO](id.toIntOption.toRight(BadRequest()))
+        _ <- EitherT.right(zookRepository.incrementDownloads(id))
+      } yield ()).value.flatMap {
+        case Left(resp) => resp
+        case Right(_) => StaticFile
+          .fromURL[IO](new URL(s"http://static.zooklabs.com/zooks/$id/$name.zook"))
+          .getOrElseF(NotFound())
+      }
+    }
   }
 
-  val endpoints: HttpRoutes[IO] = HttpRoutes.of[IO](imageEndpoint)
+  val endpoints: HttpRoutes[IO] = HttpRoutes.of[IO](imageEndpoint orElse zookEndpoint)
 }
