9bb25d94ab0bbd5bdf5f628ea8cce834c6f96ef2
BearRebel <bearrebel@zooklabs.com>
2021-11-02 22:57:14 +0000

    cleanup LoginEndpoints, rework StaticEndpoints

--- src/main/scala/zooklabs/endpoints/StaticEndpoints.scala
+++ src/main/scala/zooklabs/endpoints/StaticEndpoints.scala
@@ -1,31 +1,31 @@
 package zooklabs.endpoints
 
-import cats.data.EitherT
 import cats.effect.IO
-import org.http4s.MediaType.{Binary, Compressible, NotBinary}
+import fs2.io.file.{Path => FS2Path}
+import org.http4s.MediaType.{Compressible, NotBinary}
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.headers.{`Content-Disposition`, `Content-Type`}
 import org.typelevel.ci.CIStringSyntax
 import org.typelevel.log4cats.Logger
+import zooklabs.conf.PersistenceConfig
 import zooklabs.repository.ZookRepository
 
-import java.net.URL
-
-class StaticEndpoints(zookRepository: ZookRepository)(implicit logger: Logger[IO])
-    extends Http4sDsl[IO] {
+class StaticEndpoints(zookRepository: ZookRepository, persistenceConfig: PersistenceConfig)(implicit
+    logger: Logger[IO]
+) extends Http4sDsl[IO] {
 
   val imageEndpoint: PartialFunction[Request[IO], IO[Response[IO]]] = {
-    case GET -> Root / "image" / id =>
+    case GET -> Root / "zooks" / id / "image.png" =>
       StaticFile
-        .fromURL[IO](new URL(s"http://static.zooklabs.com/zooks/$id/image.png"))
+        .fromPath[IO](FS2Path.fromNioPath(persistenceConfig.path).resolve(s"zooks/$id/image.png"))
         .getOrElseF(NotFound())
   }
 
   val zookEndpoint: PartialFunction[Request[IO], IO[Response[IO]]] = {
     case GET -> Root / "zooks" / id / name =>
       StaticFile
-        .fromURL[IO](new URL(s"http://static.zooklabs.com/zooks/$id/$name.zook"))
+        .fromPath[IO](FS2Path.fromNioPath(persistenceConfig.path).resolve(s"zooks/$id/$name.zook"))
         .map(
           _.withContentType(
             `Content-Type`(
