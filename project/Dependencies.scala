cdef61f316788c9488638c0cd24fe7cb3564b658
Scala Steward <scala_steward@virtuslab.com>
2022-11-26 21:36:47 +0000

    Update postgresql to 42.2.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val http4sBlaze    = "0.23.12"
     val log4Cats       = "2.5.0"
     val logbackClassic = "1.4.3"
-    val postgres       = "42.2.22"
+    val postgres       = "42.2.27"
     val refined        = "0.10.1"
     val scalaJwt       = "9.1.1"
     val scalatest      = "3.2.10"
@@ -63,7 +63,7 @@ object Dependencies {
       "log4cats-slf4j"
     ).map("org.typelevel" %% _ % Version.log4Cats)
 
-      val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logbackClassic
+    val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logbackClassic
 
     val ciris: Seq[ModuleID] = Seq(
       "is.cir" %% "ciris",
@@ -89,7 +89,6 @@ object Dependencies {
       "com.github.jwt-scala" %% "jwt-core",
       "com.github.jwt-scala" %% "jwt-circe"
     ).map(_ % Version.scalaJwt)
-
 
     val munit = "org.scalameta" %% "munit" % Version.munit
   }
