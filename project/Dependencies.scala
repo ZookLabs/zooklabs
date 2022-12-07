f7194812b93642a8b3a671d117a0c903f56072b7
Scala Steward <scala_steward@virtuslab.com>
2022-12-07 12:47:07 +0000

    Revert commit(s) aa41876

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val postgres       = "42.2.22"
     val refined        = "0.10.1"
     val scalaJwt       = "9.1.1"
-    val scalatest      = "3.2.14"
+    val scalatest      = "3.2.10"
     val munit          = "0.7.29"
     val zookcore       = "1.0.2"
   }
@@ -63,7 +63,7 @@ object Dependencies {
       "log4cats-slf4j"
     ).map("org.typelevel" %% _ % Version.log4Cats)
 
-    val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logbackClassic
+      val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logbackClassic
 
     val ciris: Seq[ModuleID] = Seq(
       "is.cir" %% "ciris",
@@ -89,6 +89,7 @@ object Dependencies {
       "com.github.jwt-scala" %% "jwt-core",
       "com.github.jwt-scala" %% "jwt-circe"
     ).map(_ % Version.scalaJwt)
+
 
     val munit = "org.scalameta" %% "munit" % Version.munit
   }
