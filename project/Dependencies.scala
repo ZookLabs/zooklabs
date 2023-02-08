9a99cdd81bcfedc2077b6fd204e10eb6d3e6be6a
Scala Steward <scala_steward@virtuslab.com>
2023-02-08 21:28:54 +0000

    Update ciris, ciris-refined to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val cats               = "2.8.0"
     val catsEffect         = "3.3.14"
     val circe              = "0.14.3"
-    val ciris              = "2.4.0"
+    val ciris              = "3.1.0"
     val doobie             = "1.0.0-RC2"
     val flyway             = "9.4.0"
     val fs2                = "3.3.0"
