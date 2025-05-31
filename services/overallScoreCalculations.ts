0252a5af14d40e903e7a79a5dd1d93c79196c87a
BearRebel <56321631+BearRebel@users.noreply.github.com>
2025-05-31 22:38:38 +0100

    update logs

--- services/overallScoreCalculations.ts
+++ services/overallScoreCalculations.ts
@@ -10,14 +10,22 @@ export class OverallScoreCalculations {
 
     // Converts a position on the scale 1 to (entries) to 0 to 100
     static normaliseRank(position: number, entries: number): number {
-        return (position - 1) * (100.0 / entries);
+        console.log(`Normalising rank: position=${position}, entries=${entries}`);
+
+        const rank = position - 1; // Convert to 0-based index
+        console.log(rank);
+        const bla = (100.0 / entries)
+        console.log(bla);
+
+        const other = rank * bla
+        console.log(other);
+
+        return other;
     }
 
     static getSingleLeagueScore(position: number, entries: number): number {
         const normalised = OverallScoreCalculations.normaliseRank(position, entries);
-        console.log(normalised)
         const processNormalised = OverallScoreCalculations.processNormalised(normalised);
-        console.log(processNormalised)
         return processNormalised
     }
 
