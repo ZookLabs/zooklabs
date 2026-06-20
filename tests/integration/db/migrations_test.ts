1b1660b287149dda1de56943211112832ed97b3d
Shoeman <16366343+Shoeman@users.noreply.github.com>
2026-06-20 16:57:21 +0100

    chore: rename autotrial migration to use timestamp

--- tests/integration/db/migrations_test.ts
+++ tests/integration/db/migrations_test.ts
@@ -84,7 +84,7 @@ async function validateMigration() {
       "V7__Add_tournament_table.ts",
       "V8__Add_is_admin_column.ts",
       "V9__Add_views_and_download_columns.ts",
-      "VER010__Add_AutoTrialResults_table.ts",
+      "1781970964__Add_AutoTrialResults_table.ts",
     ],
     "users table should exist after migration",
   )
